2e3a7c69dcb9b5d6155f40bcb23a576fac6d926f
Jason Jacob <jj@Jasons-MBP.lan>
2020-04-15 09:12:38 +0930

    mess

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -5,11 +5,8 @@ set -e
 
 a=10
  
-# begin the pipeline.yml file
-echo "steps:"
-
-# add a new command step to run the tests in each test directory
-for test_dir in test/*/; do
-    echo "  - command: \"echo "do this"\""
-    echo "    label: \"extra step\""
-done
+if [[ "$a" == "10" ]]; then
+  echo "steps:"
+  echo "  - command: \"echo Deploy!\""
+  echo "    label: \":rocket:\""
+fi
