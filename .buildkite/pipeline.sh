39b01b92008c132f7989322ecbfbc476f656fb2c
Jason Jacob <jj@Jasons-MBP.lan>
2020-04-15 09:05:48 +0930

    adf

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -5,14 +5,11 @@ set -e
 
 a=10
  
-# a: variable is set
-if [[ -v a ]];
-    # begin the pipeline.yml file
-    echo "steps:"
+# begin the pipeline.yml file
+echo "steps:"
 
-    # add a new command step to run the tests in each test directory
-    for test_dir in test/*/; do
+# add a new command step to run the tests in each test directory
+for test_dir in test/*/; do
     echo "  - command: \"echo "do this"\""
     echo "    label: \"extra step\""
-    done
-fi
+done
