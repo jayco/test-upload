8ee0e03aa0265d49f5c9c2c4be39a14c6ea434c4
Jason Jacob <jj@Jasons-MBP.lan>
2020-04-15 12:51:25 +0930

    adsf

--- .buildkite/coverage.sh
+++ .buildkite/coverage.sh
@@ -5,7 +5,7 @@ set -eu
 STEP_ONE=`buildkite-agent step get "outcome" --step "step-two"`
 
 echo "HERE"
-echo "$STEP_ONE"
+echo "${STEP_ONE}"
 
 if [[ "$STEP_ONE" == "passed" ]]; then
     .buildkite/deploy-pipeline.sh | buildkite-agent pipeline upload
