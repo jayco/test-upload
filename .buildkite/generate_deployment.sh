379a3330f4c0ac98e15bd8d3b0970ee17b2a7726
Jason Jacob <jj@Jasons-MBP.lan>
2020-04-16 09:47:47 +0930

    demo

--- .buildkite/generate_deployment.sh
+++ .buildkite/generate_deployment.sh
@@ -2,8 +2,6 @@
 
 set -eu
  
-echo "HERE"
-echo `buildkite-agent step get "outcome" --step "step1"`
 
 if [[ `buildkite-agent step get "outcome" --step "step1"` == "passed" ]]; then
     .buildkite/deploy-pipeline.sh | buildkite-agent pipeline upload
