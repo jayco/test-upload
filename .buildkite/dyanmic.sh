08bdcf6dfb55cca779f0532194fb9d1010b326cb
Jason Jacob <jason@buildkite.com>
2020-08-10 18:57:10 +0930

    do it

---
+++ .buildkite/dyanmic.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# exit immediately on failure, or if an undefined variable is used
+set -eu
+
+channel="#general"
+
+if [["${BUILDKITE_BUILD_CREATOR_EMAIL}" == "test@example.com"]]; then
+    channel = "@slackuser"
+fi
+
+
+
+# begin the pipeline.yml file
+echo "notify:"
+echo "  - slack: ${channel}"
+echo "    if: build.state == \"passed\""
+echo "steps:"
+echo "  - command: echo 'do this'"
