d4f82056da45df63f865b64b3cc7e5f92a77eefa
Jason Jacob <jason@buildkite.com>
2020-08-28 16:27:45 +0930

    adsf

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -7,3 +7,10 @@ export AMI_ID=$(buildkite-agent meta-data get AMI_ID)
 
 echo "steps:"
 echo "  - command: echo \"do this\""
+
+echo "notify:"
+echo "  - slack:"
+echo "      channels:"
+echo "        - \"#bk\""
+echo "       message: \":tada: This passed for this\""
+echo "    if: build.state == \"passed\""
