f094d50cc1d7ea4d6b792650ac5c83cb1b766ed0
Jason Jacob <jason@buildkite.com>
2020-08-28 16:18:53 +0930

    format

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -5,12 +5,12 @@ set -eu
 
 export AMI_ID=$(buildkite-agent meta-data get AMI_ID)
 
-echo "notify:"
-echo " - slack:"
-echo "     channels:"
-echo "        - \"#general\""
-echo "     message: \":tada: This passed for $AMI_ID\""
-echo "   if: build.state == \"passed\""
-
 echo "steps:"
 echo "  - command: echo \"do this\""
+
+echo "notify:"
+echo "  - slack:"
+echo "      channels:"
+echo "        - \"#general\""
+echo "       message: \":tada: This passed for $AMI_ID\""
+echo "    if: build.state == \"passed\""
